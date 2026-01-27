94314ca7294e9d2b803b555dcd7f0d8040e90e4a
Harshita Dhingra <harshitadhinra7@gmail.com>
2026-01-27 12:41:15 +0530

    callstack , object methodf

---
+++ JS DAY 4/script.js
@@ -0,0 +1,9 @@
+let user={
+FullName:"Harshita Dhingra",
+address:"rewari",
+mobile:123456789,
+favcolor:['black','white','blue']
+}
+//we can access the individual value by targetting key with variable name
+console.log(user.FullName,user.mobile,user.favcolor[1])//Harshita Dhingra 123456789 white 
+
