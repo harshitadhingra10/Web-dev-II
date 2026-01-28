a7398d09a8c79fc2d91acdc87b318bad76c3f42a
Harshita Dhingra <harshitadhinra7@gmail.com>
2026-01-28 15:48:01 +0530

    JS DAY6 added

---
+++ JS DAY6/script.js
@@ -0,0 +1,6 @@
+const headingElement=document.getElementById('myheading');
+console.log(headingElement);
+
+headingElement.textContent="New Heading";
+headingElement.styleSheets.color="blue";
+headingElement.styleSheets.backgroundColor="yellow"
