2374ff2396b8990a926b7f88775b591b737748ee
Harshita Dhingra <harshitadhinra7@gmail.com>
2026-01-29 11:52:04 +0530

    JS DAY 7 added

---
+++ JS DAY 7/script.js
@@ -0,0 +1,15 @@
+// const profile=document.getElementById("profile");
+
+// profile.style.backgroundColor="tomato";
+// profile.style.padding="15px";
+// profile.style.textAlign="center"
+
+//task2
+
+const highlight=document.getElementByIdClassName("important");
+console.log(highlight)
+
+for(let i=0;i<highlight.length;i++){
+
+    highlight[i].style.color="red"
+}
